26a08e51bf06a6785ece120aca4233d7b427eaaf
s-takada <s-takada@tribalmedia.co.jp>
2019-01-15 18:29:29 +0900

    feature: set api token flag.

--- main/main.go
+++ main/main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	cw "github.com/griffin-stewie/go-chatwork"
 	"strconv"
 )
 
+var apiToken string
+
+func init() {
+	flag.StringVar(&apiToken, "token", "", "Chatwork API key")
+	flag.Parse()
+}
+
 func main() {
 
-	cw := cw.NewClient(`4d74d8bbb2c42c166156ccce2e6ab44e`)
-	fmt.Printf("Your rate limit: %+v\n", cw.RateLimit())
+	chatwork := cw.NewClient(apiToken)
+	fmt.Printf("Your rate limit: %+v\n", chatwork.RateLimit())
 
-	s, err := cw.MyStatus()
+	s, err := chatwork.MyStatus()
 	if err != nil {
 		fmt.Printf("%+v\n", err)
 	}
 	fmt.Printf("Your status: %+v\n", s)
 
-	r, err := cw.Rooms()
+	r, err := chatwork.Rooms()
 	if err != nil {
 		fmt.Printf("%+v\n", err)
 	}
@@ -26,7 +34,7 @@ func main() {
 			fmt.Printf("room name: %+v\n", r[i].Name)
 			fmt.Printf("unread unmber: %+v\n", r[i].UnreadNum)
 
-			m, err := cw.RoomMessages(strconv.Itoa(r[i].RoomID), map[string]string{"force": "1"})
+			m, err := chatwork.RoomMessages(strconv.Itoa(r[i].RoomID), map[string]string{"force": "1"})
 			if err != nil {
 				fmt.Printf("%+v\n", err)
 			}
