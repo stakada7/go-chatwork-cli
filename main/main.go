6443ffbca2eb9dba40f8786b9176c2eabe3809cd
s-takada <s-takada@tribalmedia.co.jp>
2019-01-15 16:57:02 +0900

    feature: example API call.

--- main/main.go
+++ main/main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	cw "github.com/griffin-stewie/go-chatwork"
+	"log"
+)
+
 func main() {
 
+	cw := cw.NewClient(`4d74d8bbb2c42c166156ccce2e6ab44e`)
+	log.Printf("%+v", cw.RateLimit())
+
+	m, err := cw.Me()
+	if err != nil {
+		log.Printf("%+v", err)
+	}
+	log.Printf("%+v", m)
+
+	s, err := cw.MyStatus()
+	if err != nil {
+		log.Printf("%+v", err)
+	}
+	log.Printf("%+v", s)
+
+	t, err := cw.MyTasks(map[string]string{})
+	if err != nil {
+		log.Printf("%+v", err)
+	}
+	log.Printf("%+v", t)
+
+	//c, err := cw.Contacts()
+	//if err != nil {
+	//	log.Printf("%+v", err)
+	//}
+	//log.Printf("%+v", c)
+	//
+	//r, err := cw.Rooms()
+	//if err != nil {
+	//	log.Printf("%+v", err)
+	//}
+	//log.Printf("%+v", r)
 }
